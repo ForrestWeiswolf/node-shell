b354e852bc495698b51a332d7010bf61a0c90348
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2017-07-31 10:41:28 -0400

    implement ls

--- commands.js
+++ commands.js
@@ -1,7 +1,18 @@
-module.exports.pwd = function function_name() {
+var fs = require('fs')
+
+module.exports.pwd = function() {
   process.stdout.write(process.cwd())
 }
 
-module.exports.date = function function_name() {
+module.exports.date = function() {
   process.stdout.write(new Date().toString())
+}
+
+module.exports.ls = function() {
+  fs.readdir('.', function(err, files){
+    if(err) throw err
+    files.forEach(function(file){
+      process.stdout.write(file.toString() + "\n")
+    })
+  })
 }
