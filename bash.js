c95da4d180cd170adb03a32190428bc1fa4632a9
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2017-07-31 10:35:32 -0400

    Refactor - uses bracket notation on commands instead of switch-case

--- bash.js
+++ bash.js
@@ -4,18 +4,19 @@ process.stdout.write('prompt > ')
 
 process.stdin.on('data', function (data) {
   var cmd = data.toString().trim(); // remove the newline
-
-  switch(cmd){
-    case 'pwd':
-      commands.pwd()
-      process.stdout.write('\nprompt > ')
-      break
-    case 'date':
-      commands.date()
-      process.stdout.write('\nprompt > ')
-      break
-    default:
-      throw "Invalid command"
-  }
+  commands[cmd]();
+  process.stdout.write('\nprompt > ')
+  // switch(cmd){
+  //   case 'pwd':
+  //     commands.pwd()
+  //     process.stdout.write('\nprompt > ')
+  //     break
+  //   case 'date':
+  //     commands.date()
+  //     process.stdout.write('\nprompt > ')
+  //     break
+  //   default:
+  //     throw "Invalid command"
+  // }
 
 });
