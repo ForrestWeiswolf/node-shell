34352b7b9711b013c9f8b7061a1c1a827053698a
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2017-07-31 10:15:40 -0400

    implemented stream communication and pwd

---
+++ bash.js
@@ -0,0 +1,11 @@
+process.stdin.write('prompt > ')
+
+process.stdin.on('data', function (data) {
+  var cmd = data.toString().trim(); // remove the newline
+
+  switch(cmd){
+    case 'pwd':
+      process.stdin.write(process.cwd())
+  }
+
+});
