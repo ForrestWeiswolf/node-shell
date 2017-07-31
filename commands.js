faa9ae8365ad9d90554d0938b207377628c876b0
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2017-07-31 10:48:54 -0400

    Change to object literal syntax for mudule.exports in commands.js

--- commands.js
+++ commands.js
@@ -1,18 +1,22 @@
 var fs = require('fs')
 
-module.exports.pwd = function() {
-  process.stdout.write(process.cwd())
-}
+module.exports = {
 
-module.exports.date = function() {
-  process.stdout.write(new Date().toString())
-}
+  pwd: function() {
+    process.stdout.write(process.cwd())
+  },
 
-module.exports.ls = function() {
-  fs.readdir('.', function(err, files){
-    if(err) throw err
-    files.forEach(function(file){
-      process.stdout.write(file.toString() + "\n")
+  date: function() {
+    process.stdout.write(new Date().toString())
+  },
+
+  ls: function() {
+    fs.readdir('.', function(err, files){
+      if(err) throw err
+      files.forEach(function(file){
+        process.stdout.write(file.toString() + "\n")
+      })
     })
-  })
+  }
+
 }
