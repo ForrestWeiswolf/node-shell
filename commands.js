156eb7f93268aa24e766d1cd59ce4c6ed683123d
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2017-07-31 12:00:01 -0400

    implement cat, head, tial

--- commands.js
+++ commands.js
@@ -34,5 +34,35 @@ module.exports = {
       if(err) throw err
       process.stdout.write(contents + "\n")
     })
-  }
+  },
+
+  head: function(args){
+    var filename = args[0];
+    var numLines = args[1] || 5;
+
+    fs.readFile(filename, function(err, contents){
+      if(err) throw err
+      lines = contents.toString().split('\n')
+
+      for(i = 0; i < numLines; i++){
+        process.stdout.write(lines[i] + "\n")
+      }
+    })
+  },
+
+  tail: function(args){
+    var filename = args[0];
+    var numLines = args[1] || 5;
+
+    fs.readFile(filename, function(err, contents){
+      if(err) throw err
+      lines = contents.toString().split('\n')
+
+      for(i = lines.length - numLines; i < lines.length; i++){
+        process.stdout.write(lines[i] + "\n")
+      }
+    })
+  },
+
+
 }
